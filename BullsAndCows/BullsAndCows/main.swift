1a2b5306d47462e4c64e5f8d9e3728ac560eaa90
Seungwon Lee <sam98528@naver.com>
2024-03-11 20:04:02 +0900

    숫자야구 LV2

--- BullsAndCows/BullsAndCows/main.swift
+++ BullsAndCows/BullsAndCows/main.swift
@@ -1,30 +1,90 @@
 import Foundation
 
-let numbers = [1,2,3,4,5,6,7,8,9,0]
-
-// Using randomElement()
-func generateRandomElement(_ arr : [Int]) -> [Int]{
-    var arr = arr
-    var ans : [Int] = []
-    for _ in 1...3 {
-        if let number = arr.randomElement() {
-            ans.append(number)
-            arr.remove(at: arr.firstIndex(of: number)!)
-        }else {
-            print("ERROR")
+class Game {
+    fileprivate var answer : [Int] = []
+    
+    func generateAnswer(){
+        var numbers = [1,2,3,4,5,6,7,8,9,0]
+        var ans : [Int] = []
+        for _ in 1...3 {
+            if let number = numbers.randomElement() {
+                ans.append(number)
+                numbers.remove(at: numbers.firstIndex(of: number)!)
+            }else {
+                print("ERROR")
+            }
+        }
+        answer = ans
+    }
+    
+    func startGame () {
+        print("<게임을 시작합니다>")
+        print(answer)
+        var correct = false
+        while(!correct){
+            print("숫자를 입력하세요!")
+            if let str = readLine(){
+                if !isCorrectString(str) {
+                    continue
+                }else {
+                    correct = checkAnswer(str)
+                }
+            }
+            
+            
+            print("")
         }
     }
-    return ans
-}
-
-// Using Shuffle
-func generateShuffle( _ arr: [Int]) -> [Int]{
-    var arr = arr.shuffled()
-    return arr.suffix(3)
     
+    func isCorrectString (_ input: String) -> Bool {
+        if input.count != 3 {
+            print("올바르지 않는 입력값입니다.(3자리 수)")
+            print("")
+            return false
+        }
+        
+        for char in input {
+            if !char.isNumber{
+                print("올바르지 않는 입력값입니다.(숫자만 입력가능)")
+                print("")
+                return false
+            }
+        }
+        return true
+    }
+    
+    func checkAnswer(_ input : String) -> Bool{
+        var strike = 0
+        var ball = 0
+        
+        for (index,char) in input.enumerated() {
+            let tempNum = Int(String(char))!
+            if answer.firstIndex(of: tempNum) == index {
+                strike += 1
+            }else if answer.contains(tempNum){
+                ball += 1
+            }
+        }
+        if strike == 3 {
+            print("정답입니다!")
+            return true
+        }else{
+            if ball == 0 && strike == 0 {
+                print("Nothing")
+            }else if ball == 0{
+                print("\(strike) 스트롸이크!")
+            }else if strike == 0 {
+                print("\(ball) 뽈!")
+            }else{
+                print("\(strike) 스트롸이크! \(ball) 뽈! ")
+            }
+            return false
+        }
+        
+    }
 }
 
-var answerWithRandomElement = generateRandomElement(numbers)
-var answerWithShuffle = generateShuffle(numbers)
-print(answerWithRandomElement)
-print(answerWithShuffle)
+
+let firstGame = Game()
+firstGame.generateAnswer()
+firstGame.startGame()

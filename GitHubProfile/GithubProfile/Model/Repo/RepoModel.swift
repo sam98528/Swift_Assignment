4267917a548f8274818beba558859aeaf64aa0ff
sam98528 <sam98528@naver.com>
2024-04-04 01:48:38 +0900

    Last Page Check, Over APi 호출 방지

--- GitHubProfile/GithubProfile/Model/Repo/RepoModel.swift
+++ GitHubProfile/GithubProfile/Model/Repo/RepoModel.swift
@@ -10,8 +10,10 @@ class RepoModel {
     var delegate: RepoModelDelegate?
     var urlString = "https://api.github.com/users/"
     var user : String
-    var page = 1
     var token : String = ""
+    var page = 0
+    var lastPage = Int.max
+    var checkedLastPage = false
     
     init(delegate: RepoModelDelegate? = nil, user: String, urlString: String = "https://api.github.com/users/") {
         self.delegate = delegate
@@ -54,30 +56,55 @@ class RepoModel {
     
     func getRepoAlamofire(){
         // 페이지 번호를 적용하여 URL 생성
-        print("getRepoAlamofire() ")
-        let url = URL(string: "\(urlString)?page=\(page)")!
-        let headers: HTTPHeaders = [
+        if page < lastPage{
+            print("getRepoAlamofire() ")
+            let url = URL(string: "\(urlString)?page=\(page)")!
+            let headers: HTTPHeaders = [
                 "Authorization": token
             ]
-        AF.request(url,headers: headers).response { response in
-            switch response.result {
-            case .success(let data):
-                do {
-                    let newRepos = try JSONDecoder().decode([Repo].self, from: data!)
-                    Repo.data.append(contentsOf: newRepos) // 새로운 데이터를 기존 데이터에 추가
-                    
-                    DispatchQueue.main.async {
-                        self.delegate?.reposRetrieved(repos: newRepos)
+            AF.request(url,headers: headers).response { response in
+                if !self.checkedLastPage{
+                    if let headers = response.response?.headers {
+                        if let linkHeader = headers["Link"] {
+                            let links = linkHeader.components(separatedBy: ",")
+                            for link in links {
+                                if link.contains("rel=\"last\"") {
+                                    if let pageRange = link.range(of: "page=")?.upperBound {
+                                        let nextPageIndex = link[pageRange...]
+                                            .prefix(while: { $0.isNumber })
+                                        if let pageNumber = Int(nextPageIndex) {
+                                            print(pageNumber)
+                                            self.lastPage = pageNumber
+                                            self.checkedLastPage = true
+                                            break
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                    
-                    // 다음 페이지로 이동
-                    self.page += 1
-                } catch {
-                    print("Error Parsing Json")
                 }
-            case .failure(let error):
-                print(error)
+                
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let newRepos = try JSONDecoder().decode([Repo].self, from: data!)
+                        Repo.data.append(contentsOf: newRepos) // 새로운 데이터를 기존 데이터에 추가
+                        
+                        DispatchQueue.main.async {
+                            self.delegate?.reposRetrieved(repos: newRepos)
+                        }
+                        
+                        // 다음 페이지로 이동
+                        self.page += 1
+                    } catch {
+                        print("Error Parsing Json")
+                    }
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
+        
     }
 }
